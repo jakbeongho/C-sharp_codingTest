df93c27e70aa8e6df32f2dee5233bee5a0200489
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2025-09-05 13:47:33 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.16 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        
+        int count = 0;
+        
+        while (money >=5500)
+        {
+            money = money - 5500;
+            count += 1;
+        }
+        
+        answer[0] = count;
+        answer[1] = money;
+        
+        return answer;
+    }
+}
