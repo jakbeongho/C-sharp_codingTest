e3c912bf93972981e626eb0e23432ca8677f1adc
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2025-09-13 16:53:36 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.17 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Solution {
+    public int[] solution(int n) {
+        
+        int length = 0;
+        if(n%2==0)
+        {
+            length = n/2;
+        }
+        else 
+        {
+            length = n/2+1;
+        }
+        
+        int[] answer = new int[length];
+        
+        
+        
+        for(int i=0;i<length;i++)
+        {
+            answer[i] = i*2+1;
+        }
+        
+        
+        return answer;
+    }
+}
